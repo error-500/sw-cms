b17603043b9fadc4180b750b83e1e65fbec997b5
swods <swods>
2020-11-18 22:00:08 +0300

    ааа

--- views/layouts/menu.php
+++ views/layouts/menu.php
@@ -20,15 +20,17 @@ $delivery = Yii::$app->sw->getModule('product')->group('find')
 
 <ul class="nav navbar-nav">
     <li><a href="<?= Url::to('/news') ?>">Лента <span class="caret"></span></a></li>
-   
-    <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Меню <span class="caret"></span></a>
-        <ul class="dropdown-menu multi-level">
-            <?php foreach (Yii::$app->sw->getModule('product')->group('find')->where(['parent_id' => null])->orderBy('pos ASC')->all() as $group): ?>
-                <li><a href="<?= Url::to(["/menu/{$group->tech_name}"]) ?>"><?= $group->name ?></a></li>
-            <?php endforeach ?>
-        </ul>
-    </li>
+    
+    <?php foreach (Yii::$app->sw->getModule('product')->group('find')->where(['parent_id' => null])->orderBy('pos ASC')->all() as $main_product): ?>
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"><?= $main_product->name ?> <span class="caret"></span></a>
+            <ul class="dropdown-menu multi-level">
+                <?php foreach ($main_product->groups as $group): ?>
+                    <li><a href="<?= Url::to(["/menu/{$group->tech_name}"]) ?>"><?= $group->name ?></a></li>
+                <?php endforeach ?>
+            </ul>
+        </li>
+    <?php endforeach ?>
 
     <li><a href="<?= Url::to('/delivery') ?>">Доставка <span class="caret"></span></a></li>
     <li><a href="<?= Url::to('/reservation') ?>">Бронь <span class="caret"></span></a></li>
