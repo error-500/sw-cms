e687aa0dd2fa6b2ee646984d7c81b16412157e48
Error500 <error500@error500.ru>
2021-02-15 12:52:25 +0300

    Closes: #25
    Small fixes in icons
    Update style for cart images

--- widgets/cart/views/cart.php
+++ widgets/cart/views/cart.php
@@ -4,12 +4,13 @@ use yii\helpers\Url;
 
 ?>
 <?php if ($full): ?>
-<li class="dropdown cart <?php if(!$cart): ?>invisible<?php endif; ?>">
+<li :class="{'btn-cart': true, invisible: !Boolean(cartCount)}"
+    style="margin-top: -.7em;">
     <a v-b-toggle.cart-sidebar>
-        <span class="fa-stack fa-lg">
+        <span class="fa-stack">
             <b class="fa fa-shopping-bag fa-stack-2x mr-5"></b>
-            <b class="fa fa-circle fa-stack-2x text-success ml-4"></b>
-            <b class="text-bold cart-total fa fa-stack-1x fa-inverse ml-4"
+            <b class="fa fa-circle fa-stack-2x text-success ml-3"></b>
+            <b class="text-bold cart-total fa fa-stack-1x fa-inverse ml-3"
                v-html="cartCount">
                 <?php if(!empty($cart['items'])): echo array_sum(array_column($cart['items'], 'count')); else: echo '0'; endif; ?>
             </b>
@@ -21,7 +22,8 @@ use yii\helpers\Url;
 <?php if (!$full): ?>
 <a v-b-toggle.cart-sidebar
    style="padding: 0 !important;"
-   class="css-pointer btn-cart <?php if (!$cart): ?>invisible<?php endif; ?>">
+   :class="{'css-point': true, 'btn-cart': true, invisible: !Boolean(cartCount)}"
+   style="margin-top: -.9em;">
     <span class="fa-stack fa-sm">
         <b class="fa fa-shopping-bag fa-stack-2x mr-5"></b>
         <b class="fa fa-circle fa-stack-2x text-success ml-4"></b>
