a27d50a3de3ed4d0ccd1e72812bbd86c96250069
Error500 <error500@error500.ru>
2021-03-26 16:07:26 +0300

    Fix editor in admin

--- modules/sw/modules/slider/views/item/_form.php
+++ modules/sw/modules/slider/views/item/_form.php
@@ -7,82 +7,68 @@ use yii\bootstrap4\ActiveForm;
 use app\modules\sw\modules\slider\models\Group;
 
 $button_text = sprintf('%s <i class="icon-arrow-right14 position-right"></i>', $model->isNewRecord ? 'Сохранить' : 'Обновить');
-
-Yii::$app->vueApp->data = [
-    'html_editor' => "null",
-];
-Yii::$app->vueApp->mounted = [
-    '
-    this.$set(this, "html_editor", window.ace.edit("html_editor"));
-    const textarea = document.querySelector(\'textarea[name="Item[text]"]\');
-    textarea.classList.add("d-none");
-    this.html_editor.setTheme("ace/theme/monokai");
-    this.html_editor.getSession().setMode("ace/mode/html");
-    this.html_editor.setShowPrintMargin(false);
-    this.html_editor.getSession().setValue(textarea.value);
-    this.html_editor.getSession().on(\'change\', function(){
-          textarea.value =html_editor.getSession().getValue();
-        });
-    '
-];
 ?>
 
 <div class="row">
     <div class="col-md-12">
         <?php $form = ActiveForm::begin(); ?>
-            <div class="panel panel-flat">
-                <div class="panel-body">
+        <div class="panel panel-flat">
+            <div class="panel-body">
 
-                    <?= $form->errorSummary($model) ?>
+                <?= $form->errorSummary($model) ?>
 
-                    <div class="row">
-                        <div class="col-md-4">
-                            <?= $form->field($model, 'group_id')->dropDownList(ArrayHelper::map(Group::find()->all(), 'id', 'name'), [
+                <div class="row">
+                    <div class="col-md-4">
+                        <?= $form->field($model, 'group_id')->dropDownList(ArrayHelper::map(Group::find()->all(), 'id', 'name'), [
                                 'prompt' => 'Выберите'
                             ]) ?>
-                        </div>
-
-                        <div class="col-md-2">
-                            <?= $form->field($model, 'pos')->textInput() ?>
-                        </div>
-
-                        <div class="col-md-2">
-                            <?= $form->field($model, 'active')->dropDownList([1 => 'Да', 0 => 'Нет']) ?>
-                        </div>
-
-                        <div class="col-md-4">
-                            <?= $form->field($model, 'href')->textInput(['maxlength' => true]) ?>
-                        </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-md-4">
-                            <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
-                        </div>
-
-                        <div class="col-md-4">
-                            <?= $form->field($model, 'alt')->textInput(['maxlength' => true]) ?>
-                        </div>
-
-                        <div class="col-md-4">
-                            <?= $form->field($model, 'img_obj')->fileInput(['class' => 'file-styled']) ?>
-                        </div>
+                    <div class="col-md-2">
+                        <?= $form->field($model, 'pos')->textInput() ?>
                     </div>
 
-                    <div class="row">
-                        <div class="col-md-12">
-                            <?= $form->field($model, 'text', [
-                                'inputTemplate' => '<div id="html_editor"></div> {input}'
-                            ])->textarea() ?>
-                        </div>
+                    <div class="col-md-2">
+                        <?= $form->field($model, 'active')->dropDownList([1 => 'Да', 0 => 'Нет']) ?>
                     </div>
 
-                    <div class="text-right">
-                        <?= Html::submitButton($button_text, ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+                    <div class="col-md-4">
+                        <?= $form->field($model, 'href')->textInput(['maxlength' => true]) ?>
                     </div>
-
                 </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+                    </div>
+
+                    <div class="col-md-4">
+                        <?= $form->field($model, 'alt')->textInput(['maxlength' => true]) ?>
+                    </div>
+
+                    <div class="col-md-4">
+                        <?= $form->field($model, 'img_obj')->fileInput(['class' => 'file-styled']) ?>
+                    </div>
+                </div>
+
+                <div class="row mb-2">
+                    <div class="col-md-12">
+                        <?php echo $form->field($model, 'text',[
+                                    'inputOptions' => [
+                                        'id' => 'html_editor',
+                                        'class' => 'd-none'
+                                    ]
+                                ])->textarea(); ?>
+                        <sw-code-editor selector="#html_editor"></sw-code-editor>
+                    </div>
+                </div>
+
+                <div class="text-right">
+                    <?= Html::submitButton($button_text, ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+                </div>
+
             </div>
+        </div>
         <?php ActiveForm::end(); ?>
     </div>
 </div>
